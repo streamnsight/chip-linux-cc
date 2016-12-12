64fa97ae6c968db89616aaaa6cd3f5d6e3c03066
yorele <e_leroy@hotmail.com>
2016-12-11 16:51:10 -0800

    4.4.11 update

--- 4.4.11/run.sh
+++ 4.4.11/run.sh
@@ -1,24 +1,21 @@
 #!/usr/bin/env bash
 
+if [ -e "/home/workspace/shared/config" ];
+then
+	echo "Using CONFIG file from shared folder"
+	cp /home/workspace/shared/config /home/workspace/CHIP-linux/.config
+fi
 
 cd $WORKSPACE/CHIP-linux
 # menu config
 make ARCH=arm CROSS_COMPILE=/usr/bin/arm-linux-gnueabihf- menuconfig
 
 # make kernel
-make -j9 ARCH=arm CROSS_COMPILE=/usr/bin/arm-linux-gnueabihf-
+make -j9 ARCH=arm CROSS_COMPILE=/usr/bin/arm-linux-gnueabihf- zImage dtbs
 
-# install modules
-make ARCH=arm CROSS_COMPILE=/usr/bin/arm-linux-gnueabihf- INSTALL_MOD_PATH=$WORKSPACE modules_install
-
-# patch the kernel
-cd $WORKSPACE/RTL8723BS
-for i in debian/patches/0*; do  echo $i; patch -p 1 <$i ; done
-
-# compile RTL module
-make -j9 CONFIG_PLATFORM_ARM_SUNxI=y ARCH=arm CROSS_COMPILE=/usr/bin/arm-linux-gnueabihf- -C $WORKSPACE/CHIP-linux/ M=$PWD CONFIG_RTL8723BS=m INSTALL_MOD_PATH=$WORKSPACE
-make -j9 CONFIG_PLATFORM_ARM_SUNxI=y ARCH=arm CROSS_COMPILE=/usr/bin/arm-linux-gnueabihf- -C $WORKSPACE/CHIP-linux/ M=$PWD CONFIG_RTL8723BS=m INSTALL_MOD_PATH=$WORKSPACE modules_install
-
+# build and install modules
+make -j9 ARCH=arm CROSS_COMPILE=/usr/bin/arm-linux-gnueabihf- INSTALL_MOD_PATH=$WORKSPACE modules
+make -j9 ARCH=arm CROSS_COMPILE=/usr/bin/arm-linux-gnueabihf- INSTALL_MOD_PATH=$WORKSPACE modules_install
 
 # get the name registered for this kernel from the modules directory
 export KERNEL_VERSION=$(ls $WORKSPACE/lib/modules/)
